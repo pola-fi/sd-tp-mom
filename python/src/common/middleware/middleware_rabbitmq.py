0962172f17eb7ea2b2cb732cbe96f5409a5f0073
arianlucasjarmonlisnki <arian.jarmolinski@kavak.com>
2026-04-09 07:44:31 -0300

    refactor: modularize client

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,4 +1,5 @@
 import pika
+from pika.exceptions import AMQPConnectionError, AMQPError
 
 from .middleware import (
     MessageMiddlewareQueue,
@@ -16,7 +17,8 @@ class _BlockingRabbitClient:
         self.connection = None
         self.channel = None
         self._consumer_active = False
-    
+        self.bound_consumer_queue_name = None
+
     def _reset_broken_session(self):
         self._safely_close_channel_and_connection()
         self.connection = None
@@ -27,16 +29,6 @@ class _BlockingRabbitClient:
             self.channel.close()
         if self.connection is not None and self.connection.is_open:
             self.connection.close()
-
-    def _stop_consuming_safe(self):
-        if self.channel is None or not self.channel.is_open:
-            return
-        if not self._consumer_active:
-            return
-        try:
-            self.channel.stop_consuming()
-        except Exception as exc:
-            raise MessageMiddlewareDisconnectedError() from exc
 
     def connect(self):
         try:
@@ -49,7 +41,7 @@ class _BlockingRabbitClient:
             raise MessageMiddlewareDisconnectedError() from exc
 
     @staticmethod
-    def wrap_user_callback(on_message_callback):
+    def _wrap_user_callback(on_message_callback):
         def pika_callback(ch, method, properties, body):
             def ack():
                 ch.basic_ack(delivery_tag=method.delivery_tag)
@@ -61,12 +53,10 @@ class _BlockingRabbitClient:
 
         return pika_callback
 
-    def consume_messages(
-        self, on_message_callback, *, setup_before_consume, resolve_queue_name, prefetch=None
-    ):
-        pika_cb = self.wrap_user_callback(on_message_callback)
+    def _consume_loop(self, on_message_callback, setup, resolve_queue_name, prefetch=None):
+        pika_cb = self._wrap_user_callback(on_message_callback)
         try:
-            setup_before_consume()
+            setup()
             queue_name = resolve_queue_name()
             if prefetch is not None:
                 self.channel.basic_qos(prefetch_count=prefetch)
@@ -80,18 +70,94 @@ class _BlockingRabbitClient:
                 self.channel.start_consuming()
             finally:
                 self._consumer_active = False
+        except AMQPConnectionError as exc:
+            raise MessageMiddlewareDisconnectedError() from exc
+        except AMQPError as exc:
+            raise MessageMiddlewareMessageError() from exc
+
+    def consume_from_queue(self, queue_name, on_message_callback, prefetch=None):
+        def setup():
+            self.channel.queue_declare(queue=queue_name, durable=False)
+
+        self._consume_loop(
+            on_message_callback,
+            setup,
+            lambda: queue_name,
+            prefetch,
+        )
+
+    def _declare_direct_exchange(self, exchange_name):
+        self.channel.exchange_declare(
+            exchange=exchange_name,
+            exchange_type="direct",
+            durable=False,
+        )
+
+    def consume_from_exchange(
+        self, exchange_name, routing_keys, on_message_callback
+    ):
+        def setup():
+            self._declare_direct_exchange(exchange_name)
+            result = self.channel.queue_declare(queue="", exclusive=True)
+            self.bound_consumer_queue_name = result.method.queue
+            for rk in routing_keys:
+                self.channel.queue_bind(
+                    queue=self.bound_consumer_queue_name,
+                    exchange=exchange_name,
+                    routing_key=rk,
+                )
+
+        self._consume_loop(
+            on_message_callback,
+            setup,
+            lambda: self.bound_consumer_queue_name,
+            prefetch=1,
+        )
+
+    def publish_to_queue(self, queue_name, body):
+        def op():
+            self.channel.queue_declare(queue=queue_name, durable=False)
+            self.channel.basic_publish(
+                exchange="",
+                routing_key=queue_name,
+                body=body,
+            )
+
+        self._run_publish(op)
+
+    def publish_to_exchange(self, exchange_name, routing_keys, body):
+        def op():
+            self._declare_direct_exchange(exchange_name)
+            for rk in routing_keys:
+                self.channel.basic_publish(
+                    exchange=exchange_name,
+                    routing_key=rk,
+                    body=body,
+                )
+
+        self._run_publish(op)
+
+    def _run_publish(self, operation):
+        try:
+            operation()
+        except AMQPConnectionError as exc:
+            raise MessageMiddlewareDisconnectedError() from exc
+        except AMQPError as exc:
+            raise MessageMiddlewareMessageError() from exc
+
+    def stop_consuming(self):
+        if self.channel is None or not self.channel.is_open:
+            return
+        if not self._consumer_active:
+            return
+        try:
+            self.channel.stop_consuming()
         except Exception as exc:
             raise MessageMiddlewareDisconnectedError() from exc
 
-    def send(self, publish_fn):
-        try:
-            return publish_fn()
-        except Exception as exc:
-            raise MessageMiddlewareMessageError() from exc
-
     def close(self):
         try:
-            self._stop_consuming_safe()
+            self.stop_consuming()
             self._safely_close_channel_and_connection()
         except Exception as exc:
             raise MessageMiddlewareCloseError() from exc
@@ -104,37 +170,14 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
         self._client = _BlockingRabbitClient(host)
         self._client.connect()
 
-    @property
-    def connection(self):
-        return self._client.connection
-
-    @property
-    def channel(self):
-        return self._client.channel
-
-    def _declare_listen_queue(self):
-        self._client.channel.queue_declare(queue=self.queue_name, durable=False)
-
     def start_consuming(self, on_message_callback):
-        self._client.consume_messages(
-            on_message_callback,
-            setup_before_consume=self._declare_listen_queue,
-            resolve_queue_name=lambda: self.queue_name,
-        )
+        self._client.consume_from_queue(self.queue_name, on_message_callback)
 
     def stop_consuming(self):
-        self._client._stop_consuming_safe()
+        self._client.stop_consuming()
 
     def send(self, message):
-        def _publish():
-            self._declare_listen_queue()
-            self._client.channel.basic_publish(
-                exchange="",
-                routing_key=self.queue_name,
-                body=message,
-            )
-
-        self._client.send(_publish)
+        self._client.publish_to_queue(self.queue_name, message)
 
     def close(self):
         self._client.close()
@@ -145,58 +188,25 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     def __init__(self, host, exchange_name, routing_keys):
         self.exchange_name = exchange_name
         self.routing_keys = routing_keys
-        self.queue_name = None
         self._client = _BlockingRabbitClient(host)
         self._client.connect()
 
-    @property
-    def connection(self):
-        return self._client.connection
-
-    @property
-    def channel(self):
-        return self._client.channel
-
-    def _declare_exchange(self):
-        self._client.channel.exchange_declare(
-            exchange=self.exchange_name,
-            exchange_type="direct",
-            durable=False,
-        )
-
-    def _declare_consume_topology(self):
-        self._declare_exchange()
-        result = self._client.channel.queue_declare(queue="", exclusive=True)
-        self.queue_name = result.method.queue
-        for rk in self.routing_keys:
-            self._client.channel.queue_bind(
-                queue=self.queue_name,
-                exchange=self.exchange_name,
-                routing_key=rk,
-            )
-
     def start_consuming(self, on_message_callback):
-        self._client.consume_messages(
+        self._client.consume_from_exchange(
+            self.exchange_name,
+            self.routing_keys,
             on_message_callback,
-            setup_before_consume=self._declare_consume_topology,
-            resolve_queue_name=lambda: self.queue_name,
-            prefetch=1,
         )
 
     def stop_consuming(self):
-        self._client._stop_consuming_safe()
+        self._client.stop_consuming()
 
     def send(self, message):
-        def _publish_all():
-            self._declare_exchange()
-            for rk in self.routing_keys:
-                self._client.channel.basic_publish(
-                    exchange=self.exchange_name,
-                    routing_key=rk,
-                    body=message,
-                )
-
-        self._client.send(_publish_all)
+        self._client.publish_to_exchange(
+            self.exchange_name,
+            self.routing_keys,
+            message,
+        )
 
     def close(self):
         self._client.close()
